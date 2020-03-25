9c36a02631f3a8e129c079bcfa6747f2d9c795a2
Arterius92 <artjomvangelev@gmail.com>
2020-03-25 15:47:42 +0200

    Прикрутил анимацию, доделал адаптив

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,140 @@
-$('.home_slider').slick({
-  fade: true,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  infinite: false,
-  arrows: true
-});
+$(function() {
+  $('.home_slider').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: false,
+    arrows: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    speed: 900,
+    responsive: [
+      {
+        breakpoint: 719,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
 
-$('.service_slider').slick({
-  fade: false,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  infinite: true,
-  arrows: false,
-  dots: true,
-  vertical: true,
-  verticalSwiping: true
-});
+  $('.service_slider').slick({
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    arrows: false,
+    dots: true,
+    vertical: true,
+    verticalSwiping: true
+  });
 
-$('.work_item .item_img').fancybox({
-  loop: true,
-  buttons: [
-    'zoom',
-    'share',
-    'slideShow',
-    'fullScreen',
-    'download',
-    'thumbs',
-    'close'
-  ]
-});
+  $('.work_item .item_img').fancybox({
+    loop: true,
+    buttons: [
+      'zoom',
+      'share',
+      'slideShow',
+      'fullScreen',
+      'download',
+      'thumbs',
+      'close'
+    ]
+  });
 
-$('.text_block').slick({
-  fade: false,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  infinite: true,
-  arrows: false,
-  dots: true,
-  asNavFor: '.image_block'
-});
+  $('.text_block').slick({
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    arrows: false,
+    dots: true,
+    asNavFor: '.image_block'
+  });
 
-$('.image_block').slick({
-  fade: false,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  infinite: true,
-  arrows: false,
-  dots: false,
-  rtl: true,
-  asNavFor: '.text_block'
-});
+  $('.image_block').slick({
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    rtl: true,
+    asNavFor: '.text_block'
+  });
 
-// company_slider--------------------
+  // company_slider--------------------
 
-$('.slider1').slick({
-  autoplay: true,
-  autoplaySpeed: 4000,
-  infinite: true,
-  arrows: false,
-  dots: false,
-  slidesToShow: 5,
-  speed: 1500
-});
+  $('.slider1').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    speed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 
-$('.slider2').slick({
-  autoplay: true,
-  autoplaySpeed: 4000,
-  infinite: true,
-  arrows: false,
-  dots: false,
-  slidesToShow: 5,
-  speed: 1500
-});
+  $('.slider2').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    speed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 
-// button up-------------------------
+  // button up-------------------------
 
-let btn = $('.btn_up');
-let triangle = $('.triangle');
+  let btn = $('.btn_up');
+  // let triangle = $('.triangle');
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 300) {
-    $(btn).addClass('show');
-	} else {
-    $(btn).removeClass('show');
-	}
-});
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $(btn).addClass('show');
+    } else {
+      $(btn).removeClass('show');
+    }
+  });
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '300');
-});
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
 
-// triangle show--------------------
+  // triangle show--------------------
 
-$(window).load(function() {
-  $(triangle).addClass('active');
+  // $(window).load(function() {
+  //   $(triangle).addClass('active');
+  // });
+
+  new WOW().init();
 });
